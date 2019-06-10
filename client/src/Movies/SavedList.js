970103f164442230c33a414e050d8336f3acc7d6
Karim B <mattiakarim@live.it>
2019-06-10 15:40:47 +0200

    Completed last stretch goal

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -7,11 +7,16 @@ export default class SavedList extends Component {
   }
 
   render() {
+    console.log(this.props)
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <Link 
+            to={`/movies/${movie.id}`} 
+            className="saved-movie" 
+            key={movie.id}
+          >{movie.title}</Link>
         ))}
         <Link to="/" >
           <div className="home-button">Home</div>
